d787945880826d01549ee89d272427557e815661
JuliaAme <kljukv@gmail.com>
2024-04-26 16:28:44 +0300

    Function read

--- sudoku.js
+++ sudoku.js
@@ -1,10 +1,30 @@
-function read() {
+const fs = require('fs')
+
+function read(num) {
+  const read1 = fs.readFileSync("./puzzles.txt", "utf-8").split('\n')
+  
+  let element = read1.map((el)=> el.split(','))
+  let element2 = element.map((el) => el[0].match(/.{9}/g))
+
+  for (let i = 0; i < element2.length; i++) {
+    for (let index = 0; index <  element2[i].length; index++) {
+     element2[i][index] = element2[i][index].split("");
+      
+    }
+    
+   
+    
+  }
+  return element2[num];
+  
   /**
    * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
    */
+//console.log(element);
 }
 
-function solve() {
+function solve(read) {
+
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции read.
    * Возвращает игровое поле после попытки его решить.
